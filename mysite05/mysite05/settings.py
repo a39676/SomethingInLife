ff7652224e8bcbb4cdf735d85d4cc962ca754504
恐怖分子 <396767101@qq.com>
2016-10-05 09:23:28 +0800

    start an other app to clear my finance

--- mysite05/mysite05/settings.py
+++ mysite05/mysite05/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'mysite05.polls', 
     # 'mysite05.polls.app.Polls2Config',  reference polls.apps.py
+    'mysite05.financeClear',
     'blog',
     'django.contrib.admin',
     'django.contrib.auth',
--- mysite05/mysite05/urls.py
+++ mysite05/mysite05/urls.py
@@ -6,6 +6,7 @@ from mysite05.views import hello, current_datetime
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('mysite05.polls.urls')),
+    url(r'^financeClear/', include('mysite05.financeClear.urls')),
     url(r'^hello/', hello),
     url(r'^time/', current_datetime),
     
